57a6693a16847a7ba5a4281efeca56fb4055acab
nalangekrushna <nalangekrushna@gmail.com>
2022-03-11 09:44:52 +0530

    added security group module

--- main.tf
+++ main.tf
@@ -15,9 +15,23 @@ module "vpc" {
   }
 }
 
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.8.0"
+  name = "eks-test-sg"
+  vpc_id = module.vpc.vpc_id
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_ipv6_cidr_blocks = ["::/0"]
+  ingress_rules       = ["http-80-tcp","ssh-tcp"]  
+  egress_rules = [ "all-all" ]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_ipv6_cidr_blocks = ["::/0"]
+}
+
 module "vpc_endpoints" {
   source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   vpc_id = module.vpc.vpc_id
+  security_group_ids = [module.security-group.security_group_id]
   endpoints = {
     s3 = {
       service = "s3"
