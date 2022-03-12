f9b68be7fdfe7811983e5c6442f465277c0d2deb
nalangekrushna <nalangekrushna@gmail.com>
2022-03-12 10:07:51 +0530

    added required subnets for eks

--- main.tf
+++ main.tf
@@ -16,13 +16,25 @@ module "vpc" {
   source             = "terraform-aws-modules/vpc/aws"
   version            = "3.12.0"
   name               = "eks_test_vpc"
-  enable_nat_gateway = false
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
   cidr               = var.cidr
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/elb"              = 1
   }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/internal-elb"     = 1
+  }
+
+  tags = local.tags
 }
 
 module "security-group" {
