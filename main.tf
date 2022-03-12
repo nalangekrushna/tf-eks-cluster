22de91d44f7319d84fbfea024ae2a37c9754b5c0
nalangekrushna <nalangekrushna@gmail.com>
2022-03-12 09:33:06 +0530

    added eks fargate module

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
+locals {
+  name            = "ex-${replace(basename(path.cwd), "_", "-")}"
+  cluster_version = "1.21"
+
+  tags = {
+    Example = local.name
+  }
+}
+
+
 module "vpc" {
   source             = "terraform-aws-modules/vpc/aws"
   version            = "3.12.0"
@@ -16,21 +26,21 @@ module "vpc" {
 }
 
 module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.8.0"
-  name = "eks-test-sg"
-  vpc_id = module.vpc.vpc_id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  source                   = "terraform-aws-modules/security-group/aws"
+  version                  = "4.8.0"
+  name                     = "eks-test-sg"
+  vpc_id                   = module.vpc.vpc_id
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
   ingress_ipv6_cidr_blocks = ["::/0"]
-  ingress_rules       = ["http-80-tcp","ssh-tcp"]  
-  egress_rules = [ "all-all" ]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-  egress_ipv6_cidr_blocks = ["::/0"]
+  ingress_rules            = ["http-80-tcp", "ssh-tcp"]
+  egress_rules             = ["all-all"]
+  egress_cidr_blocks       = ["0.0.0.0/0"]
+  egress_ipv6_cidr_blocks  = ["::/0"]
 }
 
 module "vpc_endpoints" {
-  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  vpc_id = module.vpc.vpc_id
+  source             = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  vpc_id             = module.vpc.vpc_id
   security_group_ids = [module.security-group.security_group_id]
   endpoints = {
     s3 = {
@@ -43,4 +53,105 @@ module "vpc_endpoints" {
     Project  = "Secret"
     Endpoint = "true"
   }
+}
+
+module "eks" {
+  source                          = "terraform-aws-modules/eks/aws"
+  version                         = "18.9.0"
+  cluster_name                    = local.name
+  cluster_version                 = local.cluster_version
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts        = "OVERWRITE"
+    }
+  }
+
+  cluster_encryption_config = [{
+    provider_key_arn = aws_kms_key.eks.arn
+    resources        = ["secrets"]
+  }]
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    example = {
+      desired_size = 1
+
+      instance_types = ["t3a.micro"]
+      labels = {
+        Example    = "managed_node_groups"
+        GithubRepo = "terraform-aws-eks"
+        GithubOrg  = "terraform-aws-modules"
+      }
+      tags = {
+        ExtraTag = "example"
+      }
+    }
+  }
+
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "backend"
+          labels = {
+            Application = "backend"
+          }
+        },
+        {
+          namespace = "default"
+          labels = {
+            WorkerType = "fargate"
+          }
+        }
+      ]
+
+      tags = {
+        Owner = "default"
+      }
+
+      timeouts = {
+        create = "20m"
+        delete = "20m"
+      }
+    }
+
+    secondary = {
+      name = "secondary"
+      selectors = [
+        {
+          namespace = "default"
+          labels = {
+            Environment = "test"
+            GithubRepo  = "terraform-aws-eks"
+            GithubOrg   = "terraform-aws-modules"
+          }
+        }
+      ]
+      subnet_ids = [module.vpc.private_subnets[1]]
+
+      tags = {
+        Owner = "secondary"
+      }
+    }
+  }
+
+  tags = local.tags
+}
+
+resource "aws_kms_key" "eks" {
+  description             = "EKS Secret Encryption Key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = local.tags
 }
